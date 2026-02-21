254d9936dd7c046585edd1b3d1250aa42753d061
mr2771-sys <mr2771@srmist.edu.in>
2026-02-21 17:01:22 +0530

    Added demo2.java

---
+++ demo2.java
@@ -0,0 +1,12 @@
+public class OOPSBannerApp{
+	public static void main(String[]argd){
+		System.out.println(" ***      ***   *****   ***** ");
+        System.out.println("*   *    *   *  *    *  *    *");
+        System.out.println("*   *    *   *  *    *  *    *");
+        System.out.println("*   *    *   *  *****   ***** ");
+        System.out.println("*   *    *   *  *       *     ");
+        System.out.println("*   *    *   *  *       *     ");
+        System.out.println(" ***      ***   *       *     ");
+
+    }
+}		
