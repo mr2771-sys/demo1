4d7be2ebf4cd2790a9ce3a9023e571c6c465bb22
mr2771-sys <mr2771@srmist.edu.in>
2026-02-22 14:29:06 +0530

    Update demo3.java

--- demo3.java
+++ demo3.java
@@ -2,7 +2,7 @@ public class Demo2 {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "***", "   ***", "  *****", "  *****"));
+        System.out.println(String.join(" ", "***", "   ***", "  *****", "   ****"));
         System.out.println(String.join(" ", "*   *", " *   *", " *    *", " *    *"));
         System.out.println(String.join(" ", "*   *", " *   *", " *    *", " *    *"));
         System.out.println(String.join(" ", "*   *", " *   *", " *****", "  *****"));
